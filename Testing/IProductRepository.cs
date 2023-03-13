d28ee6b0dd6062b0029df60857c97c294960d51d
Kenneth Baker <spyksaturn@gmail.com>
2023-03-13 14:07:24 -0400

    Intro Done

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Testing.Models;
+using System.Linq;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
